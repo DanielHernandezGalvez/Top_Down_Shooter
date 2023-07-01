13ac10d873166cdb6208b64a049efe5bc4f5b4a6
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-07-01 17:42:28 -0600

    Recordatorios

--- TodoList/App.jsx
+++ TodoList/App.jsx
@@ -6,49 +6,9 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Section,
-  Image,
-} from 'react-native';
-
+import Splash from './screens/Splash';
 function App() {
-  return (
-    <SafeAreaView style={styles.body}>
-      <View>
-        <Image style={styles.logo} source={require('./assets/todo.png')} />
-      </View>
-      <View>
-        <Text style={styles.text}>To do list</Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <Splash />;
 }
-
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: '#ddd',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {},
-  logo: {
-    width: 200,
-    height: 200,
-  },
-  text: {
-    fontSize: 40,
-    marginTop: 20,
-    fontWeight: 'bold',
-    color: '#1a1a1a',
-  },
-});
 
 export default App;
---
+++ TodoList/src/utils/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA1ZWQpuQ7s3EJ2RB5QyTGoP5TGvI4rC_8',
+  authDomain: 'recordatorios-e0b45.firebaseapp.com',
+  projectId: 'recordatorios-e0b45',
+  storageBucket: 'recordatorios-e0b45.appspot.com',
+  messagingSenderId: '649073737046',
+  appId: '1:649073737046:web:d59a1be12422d7fde9eaf7',
+};
