8abe86e4e6b6024aa9b0ee2bbbc0cdedbe3c2a74
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-07-01 17:48:52 -0600

    firebase

--- TodoList/src/utils/firebase.js
+++ TodoList/src/utils/firebase.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyA1ZWQpuQ7s3EJ2RB5QyTGoP5TGvI4rC_8',
   authDomain: 'recordatorios-e0b45.firebaseapp.com',
@@ -6,3 +12,6 @@ const firebaseConfig = {
   messagingSenderId: '649073737046',
   appId: '1:649073737046:web:d59a1be12422d7fde9eaf7',
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
