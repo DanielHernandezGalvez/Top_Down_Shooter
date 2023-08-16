e82f3662747001c138051dbd891c5d1f4671943f
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-16 11:09:02 -0600

    api call

--- nasa-app/src/utils/fetch.ts
+++ nasa-app/src/utils/fetch.ts
@@ -1,2 +1,18 @@
 const API_KEY = "wojw7kia0pN7o2S6zaVUwhBDR5OHE8FRChQk8OLh";
 const API_URL = "https://api.nasa.gov/planetary/apod";
+
+export default async (urlParams?: string) => {
+  try {
+    const response = await fetch(
+      `${API_URL}?api_key=${API_KEY}${
+        typeof urlParams !== "undefined" && urlParams?.length > 0
+          ? urlParams
+          : ""
+      }`
+    );
+    const data = await response.json();
+    return Promise.resolve(data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
