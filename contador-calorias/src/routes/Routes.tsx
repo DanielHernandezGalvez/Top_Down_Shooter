ce11792a55324615005c4ec1bad128b836241b5b
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-08-19 10:33:54 -0600

    basic structure

---
+++ contador-calorias/src/routes/Routes.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { RootStackParamsList } from "../types";
+const Stack = createNativeStackNavigator<RootStackParamsList>();
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
---
+++ contador-calorias/src/views/AddFood/AddFoot.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const AddFoot = () => {
+  return (
+    <View>
+      <Text>Add Food</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default AddFoot;
