0d181850aae5e95f696361451bf23e867c425bf8
unknown <hernandezgalvezalejandro@gmail.com>
2024-07-29 13:00:59 -0600

    languages with image

--- movies/App.js
+++ movies/App.js
@@ -1,29 +1,72 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 import icon from "./assets/icon.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={icon}
-        style={{
-          width: 100,
-          height: 100,
-          resizeMode: "contain",
-        }}
-      />
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: "https://logodownload.org/wp-content/uploads/2022/04/javascript-logo-1.png",
+          }}
+          // blurRadius={9}
+          style={{
+            width: 215,
+            height: 294,
+            resizeMode: "contain",
+          }}
+        />
+        <Text style={{ color: "#fff" }}>
+          Open up App.js to start working on your app!
+        </Text>
+
+        <Image
+          source={{
+            uri: "http://pngimg.com/uploads/php/php_PNG27.png",
+          }}
+          style={{
+            width: 215,
+            height: 294,
+            resizeMode: "contain",
+          }}
+        />
+        <Text style={{ color: "#fff" }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Image
+          source={{
+            uri: "https://brandslogos.com/wp-content/uploads/images/large/python-logo.png",
+          }}
+          style={{
+            width: 215,
+            height: 294,
+            resizeMode: "contain",
+          }}
+        />
+        <Text style={{ color: "#fff" }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="light" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#a2a2a2",
+    backgroundColor: "#2a2a2a",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 50,
+    paddingBottom: 50
   },
 });
