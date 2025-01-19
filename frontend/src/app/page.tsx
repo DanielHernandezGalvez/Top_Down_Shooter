7de6ea0a3dd0161fb42a877458cb43b2bbdc4ba9
unknown <hernandezgalvezalejandro@gmail.com>
2025-01-19 14:04:21 -0600

    frontend init

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,11 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center">
-        <h1 className="text-3xl text-stone-700 font-bold underline">
-          Hello world!
-        </h1>
-      </div>
+      <h1 className="text-stone-600 font-bold">Hello world</h1>
     </>
   );
 }
